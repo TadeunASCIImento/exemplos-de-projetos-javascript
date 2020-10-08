7c7c750dbaaad749f1eef8b27d446a1b7a81ff7b
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-10-07 22:13:41 -0300

    Definindo o layout,elementos e comprtamento da agenda de serviços.

--- js/formulario.js
+++ js/formulario.js
@@ -7,11 +7,14 @@ confirmar.addEventListener('click',function(event){
     event.preventDefault();
     console.log('Evento click do botão capturado.');
 
-    const formulario = document.querySelector(".form-agendamento");
-        
+    const formulario = document.querySelector(".form-agendamento");    
     const propietario = obterProprietario(formulario);
     const veiculo = obterVeiculo(formulario);
     const servico = obterServico(formulario);
+    let trServico = novaTr(veiculo,servico);
+    let tabela = document.querySelector("#servicos-agendados");
+    tabela.appendChild(trServico);
+    formulario.request();
     
     console.log(propietario);
     console.log(veiculo);
@@ -19,6 +22,32 @@ confirmar.addEventListener('click',function(event){
 
 
 });
+// monta linha do novo serviço na tabela.
+function novaTr(veiculo,servico){
+    let trServico = document.createElement("tr");
+    trServico.appendChild(novaTd(veiculo.marca,"td-marca"));
+    trServico.appendChild(novaTd(veiculo.modelo,"td-modelo"));
+    trServico.appendChild(novaTd(veiculo.placa,"td-placa"));
+    trServico.appendChild(novaTd(veiculo.quilometros,"td-quilometros"));
+    trServico.appendChild(novaTd(veiculo.cor,"td-cor"));
+    trServico.appendChild(novaTd(veiculo.ano,"td-ano"));
+    trServico.appendChild(novaTd(veiculo.observacoes,"td-obs-veiculo"));
+    trServico.appendChild(novaTd(servico.tipo,"td-tipo"));
+    trServico.appendChild(novaTd(servico.descricao,"td-descricao"));
+    trServico.appendChild(novaTd(servico.inicio,"td-inicio"));
+    trServico.appendChild(novaTd(servico.fim,"td-fim"));
+    trServico.appendChild(novaTd(servico.observacoes,"td-obs-servico"));
+    return trServico;
+
+}
+
+function novaTd(dado,classe){
+    let td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+
+}
 
 // obtem dados do proprietário.
 function obterProprietario(formulario){
@@ -40,6 +69,7 @@ function obterVeiculo(formulario){
         placa:formulario.placa.value,
         quilometros:formulario.quilometros.value,
         cor:formulario.cor.value,
+        ano:formulario.ano.value,
         observacoes:formulario.observacoes.value,
      }
      return veiculo;
